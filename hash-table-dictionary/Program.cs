397285963d3da4173da563ca220fc52b5ab8abb3
varvara-chornomorets <vchornomorets@kse.org.ua>
2023-03-30 13:16:11 +0300

    added actual dictionary, performed some  basic steps, that will allow us to read it

--- hash-table-dictionary/Program.cs
+++ hash-table-dictionary/Program.cs
@@ -1,8 +1,26 @@
 ﻿using hash_table_dictionary;
 using KeyValuePair = hash_table_dictionary.KeyValuePair;
 
-
 var myDictionary = new StringsDictionary();
+int counter = 0;
+string[] lines = File.ReadAllLines("C:/Users/chorn/Desktop/it/hash-table-dictionary/hash-table-dictionary/dictionary.txt");
+foreach (var line in lines)
+{
+    counter++;
+    var wordDefArray = line.Split(";");
+    string word = wordDefArray[0];
+    string defenition = string.Join("", wordDefArray, 1, wordDefArray.Length - 1);
+    myDictionary.Add(word, defenition);
+    if (counter > 150)
+    {
+        break;
+    }
+}
+myDictionary.PrintDictionary();
+Console.WriteLine(myDictionary.Get("ABASE"));
+
+
+/* var myDictionary = new StringsDictionary();
 myDictionary.Add("varia", "sasha");
 myDictionary.Add("sasha", "varia");
 myDictionary.Add("liza", "taras");
