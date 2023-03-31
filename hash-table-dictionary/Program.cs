617d58dab586001c32afc29d09150fc5de242b8c
istepmother <imachukha@kse.org.ua>
2023-03-31 09:00:10 +0200

    minor fixes

--- hash-table-dictionary/Program.cs
+++ hash-table-dictionary/Program.cs
@@ -9,22 +9,22 @@ foreach (var line in lines)
     counter++;
     var wordDefArray = line.Split(";");
     string word = wordDefArray[0];
-    string defenition = string.Join("", wordDefArray, 1, wordDefArray.Length - 1);
-    myDictionary.Add(word, defenition);
+    string definition = string.Join("", wordDefArray, 1, wordDefArray.Length - 1);
+    myDictionary.Add(word, definition);
 }
-Console.WriteLine("hello! this is dictionary! amazing, isn't it? you can type in some word, and I will give you a defenition");
+Console.WriteLine("hello! this is dictionary! amazing, isn't it? you can type in some word, and I will give you a definition");
 while (true)
 {
     string input = Console.ReadLine();
     string requestedWord = input.ToUpper();
     try
     {
-        string defenition = myDictionary.Get(requestedWord);
-        Console.WriteLine($"{requestedWord}   {defenition}");
+        string definition = myDictionary.Get(requestedWord);
+        Console.WriteLine($"{requestedWord}   {definition}");
     }
     catch
     {
-        Console.WriteLine("seems, like you have entered incorrect word");
+        Console.WriteLine("there's no such word in a dictionary");
     }
 }
 
